98e43f85b8168d3db5a838cbbf180e00c5137131
Pawsanae <aujjima.i30@gmail.com>
2025-06-17 15:32:35 +0700

    add method

--- main.py
+++ main.py
@@ -36,10 +36,29 @@ class ItemDto(BaseModel):
 items: dict[int, Item] = {}
 id = 0
 
-@app.post("/items/", response_model=Item)
+@app.post("/items/", response_model=Item, tags=["items"])
 def create_item(item: ItemDto):
     global id
     id += 1
-    
     items[id] = Item(item_id=id, name=item.name, description=item.description, price=item.price)
     return items[id]
+
+@app.get("/items/{item_id}", response_model=Item |dict, tags=["items"])
+def read_item(item_id: int):
+    return items.get(item_id, {"error": "Item not found"})
+
+@app.put("/items/{item_id}", response_model=Item | dict, tags=["items"])
+def update_item(item_id: int, item: ItemDto):
+    if item_id not in items:
+        return {"error": "Item not found"}
+    items[item_id] = Item(item_id=item_id, name=item.name, description=item.description, price=item.price)
+    return items[item_id]
+
+
+@app.delete("/items/{item_id}", response_model=dict, tags=["items"])
+def delete_item(item_id: int):
+    if item_id not in items:
+        return {"error": "Item not found"}
+    del items[item_id]
+    return {"message": "Item deleted"}
+
